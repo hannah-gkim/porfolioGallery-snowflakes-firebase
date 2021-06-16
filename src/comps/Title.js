14530d0cb35c1505166c168248a886b8870b4ff3
hannahk0108 <hannahk0108@gmail.com>
2021-06-15 21:59:06 -0500

    editted

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>HannahGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>If you can dream it, you can do it - Walt Disney</p>
     </div>
   );
 };
