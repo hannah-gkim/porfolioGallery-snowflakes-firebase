b760e85900c801dc8a1c91732c2bb00c7819ad55
hannahk0108 <hannahk0108@gmail.com>
2021-06-24 20:48:38 -0500

    removed gallery from navbar, Linked hannah gallery to a photo

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -8,11 +8,8 @@ export default function Navbar() {
         <h1>Gallery Name</h1>
       </Link>
 
-      <nav >
+      <nav>
         <h2>bio</h2>
-        <Link to="/hannah">
-          <h2>Gallery</h2>
-        </Link>
 
         <h2>instagram Logo</h2>
         <button>Contact</button>
