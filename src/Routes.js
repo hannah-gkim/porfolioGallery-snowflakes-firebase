26b602d847f2726b1d824375323e762bdaaa0379
hannahk0108 <hannahk0108@gmail.com>
2021-06-27 00:55:17 -0500

    feat: login logout admin page setup

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import LandingPage from "./comps/LandingPage";
 import HannahGallery from "./comps/HannahGallery";
 import LandingPage from "./comps/LandingPage";
-import { AuthProvider } from "./hooks/AuthContext";
 import Login from "./comps/Login";
-
+import { AuthProvider } from "./hooks/AuthContext";
+import PrivateRoute from "./comps/PrivateRoute";
+import AdminPage from "./comps/AdminPage";
 function Routes() {
   return (
     <div>
       <AuthProvider>
         <Switch>
+          <PrivateRoute exact path="/admin" component={AdminPage} />
           <Route exact path="/hannah" component={HannahGallery} />
           <Route exact path="/login" component={Login} />
           <Route path="/" component={LandingPage} />
--- src/comps/Login.js
+++ src/comps/Login.js
@@ -19,7 +19,7 @@ export default function Login() {
       setError("");
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
-      history.push("/");
+      history.push("/admin");
     } catch {
       setError("Failed to sign in");
     }
